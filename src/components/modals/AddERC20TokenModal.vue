f6b5006605f56d97fdec5c3f222609cc06606c6d
Ayagoumi <agoumi94@gmail.com>
2023-01-31 01:57:46 +0100

    Fix add token functionality

--- src/components/modals/AddERC20TokenModal.vue
+++ src/components/modals/AddERC20TokenModal.vue
@@ -67,7 +67,7 @@ export default class AddERC20TokenModal extends Vue {
         }
         try {
             //@ts-ignore
-            var tokenInst = new web3.eth.Contract(ERC20Abi.abi, val)
+            var tokenInst = new web3.eth.Contract(ERC20Abi, val)
             let name = await tokenInst.methods.name().call()
             let symbol = await tokenInst.methods.symbol().call()
             let decimals = await tokenInst.methods.decimals().call()
