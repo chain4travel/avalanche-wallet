9dd0a331d55a9a895d46ce4671c7e0cc8ae39ebc
peak3d <pfau@peak3d.de>
2023-02-03 15:36:45 +0100

    Remove GetBalance approach for P because we need to fetch UTXO either way

--- src/store/modules/network/network.ts
+++ src/store/modules/network/network.ts
@@ -153,9 +153,6 @@ const network_module: Module<NetworkState, RootState> = {
             await dispatch('Assets/onNetworkChange', net, { root: true })
             await dispatch('Launch/onNetworkChange', net, { root: true })
             dispatch('Assets/updateUTXOs', null, { root: true })
-            if (state.depositAndBond) {
-                dispatch('Assets/getPChainBalances', null, { root: true })
-            }
             dispatch('Platform/update', null, { root: true })
             dispatch('Platform/updateMinStakeAmount', null, { root: true })
             dispatch('updateTxFee')
