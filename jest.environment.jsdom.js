1b5c8b30d994a17b0b6c994aedb5d64e6c5e08ba
Chriserus <krzysborowiecki97@gmail.com>
2023-03-23 16:40:49 +0100

    Revert jest.environment.jsdom.js

--- jest.environment.jsdom.js
+++
@@ -1,8 +0,0 @@
-const JsdomEnvironment = require('jest-environment-jsdom').default
-
-export default class CustomJsdomEnvironment extends JsdomEnvironment {
-    constructor(config, context) {
-        super(config, context)
-        this.global.Uint8Array = Uint8Array
-    }
-}
