109ca51237bbd5f4b7ac3eb29a2c2e0551807586
peak3d <pfau@peak3d.de>
2023-04-02 14:18:22 +0200

    [UI] Add Ellipsis component for ellipsis clipping in the middle

---
+++ src/components/misc/Ellipsis.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="eps_row" @click.prevent="copyFullText" :pointer="!copy">
+        <span class="eps_prefix">{{ prefix }}</span>
+        <span class="eps_left">{{ left }}</span>
+        <span class="eps_right">{{ right }}</span>
+        <fa v-if="copy" class="eps_copy" icon="copy" @click.prevent="copyText"></fa>
+    </div>
+</template>
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component
+export default class Ellipsis extends Vue {
+    @Prop() text!: string
+    @Prop({ default: 0 }) prefixPos!: number
+    @Prop() copy!: boolean
+
+    get prefix(): string {
+        return this.text.slice(0, this.prefixPos)
+    }
+
+    get left(): string {
+        const count = (this.text.length - this.prefixPos) >> 1
+        return this.text.slice(this.prefixPos, count)
+    }
+
+    get right(): string {
+        const count = (this.text.length - this.prefixPos) >> 1
+        return this.text.slice(this.prefixPos + count)
+    }
+
+    copyText() {
+        navigator.clipboard.writeText(this.text).then(() => {
+            this.$store.dispatch('Notifications/add', {
+                title: ' Copied',
+                message: 'Copied to clipboard.',
+            })
+        })
+    }
+
+    copyFullText() {
+        if (!this.copy) this.copyText()
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.eps_row {
+    overflow-x: hidden;
+    display: inline-grid;
+    grid-template-columns: auto 1.1fr 0.9fr auto;
+
+    &[pointer] {
+        cursor: pointer;
+    }
+}
+
+.eps_prefix {
+    white-space: nowrap;
+}
+
+.eps_left {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    text-align: right;
+}
+
+.eps_right {
+    overflow: hidden;
+    word-break: break-all;
+    direction: rtl;
+    height: 1rem;
+}
+
+.eps_copy {
+    margin-left: 4px;
+    margin-top: auto;
+    margin-bottom: auto;
+    cursor: pointer;
+}
+</style>
