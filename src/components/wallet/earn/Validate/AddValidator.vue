e59a832a2c3b125b1efa6e3a586905e151c7cd82
Ayagoumi <agoumi94@gmail.com>
2023-04-12 12:17:02 +0000

    fix: diactivate auto fill max amount for input except for validator input

--- src/components/wallet/earn/Validate/AddValidator.vue
+++ src/components/wallet/earn/Validate/AddValidator.vue
@@ -204,6 +204,8 @@ export default class AddValidator extends Vue {
     mounted() {
         this.rewardSelect('local')
         this.validateReadyValidator()
+        //@ts-ignore
+        this.$refs.avaxinput.maxOut()
     }
 
     async validateReadyValidator() {
