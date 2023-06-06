e3e885a6e605b5c219d02fde422aafbafe889c8f
Ayoub Agoumi <agoumi94@gmail.com>
2023-06-06 19:46:19 +0100

    HOT FIX: revert webpack changes (#279)

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,21 +1,13 @@
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 
-const publicPath = process.env.PUBLIC_PATH
-
 module.exports = merge(common, {
     mode: 'development',
     devtool: 'inline-source-map',
 
-    output: {
-        publicPath: publicPath,
-        filename: 'js/[name].[fullhash:8].js',
-        chunkFilename: 'js/[name].[fullhash:8].js',
-    },
-
-    performance: {
-        hints: 'warning',
-        maxEntrypointSize: 512000,
-        maxAssetSize: 512000,
+    devServer: {
+        port: 5003,
+        historyApiFallback: true,
+        static: './dist',
     },
 })
